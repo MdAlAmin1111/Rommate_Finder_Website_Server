47ea472179258ddba27e5af3fcb292c84672cfe5
Md Al-Amin <mdalamin.iubian@gmail.com>
2025-09-01 19:24:07 +0600

    added dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -9,9 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://findMyRoomie-user:PvQjbMIlqo3aq66F@cluster0.2vavvcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2vavvcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
